2d422b91125783e26bb19c4efe0913417d71e8a6
fvilca <fvilca@uc.cl>
2023-09-25 11:27:43 -0300

    creando codigo y cargando datos

---
+++ codigo.R
@@ -0,0 +1,15 @@
+# Tarea 02 -----
+# 
+# Tópicos Aplicados a Estadística - EYP3407
+# 
+# Francisca Vilca Sanchez
+
+# PAQUETES A USAR
+
+library(ISLR)
+library(dplyr)
+library(rio)
+
+# DATOS A UTILIZAR -----
+
+datos <- Hitters
